886ce2fd4a2cd5a0bc9186cb2aebbc010a2d724a
adityadash300 <adityadash300@gmail.com>
2024-02-25 21:22:30 +0530

    added unit tests

---
+++ sync/handler_test.go
@@ -0,0 +1,10 @@
+package sync
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPacketSyncHandler_Handler(t *testing.T) {
+	fmt.Println("Sync Handler Testing")
+}
---
+++ sync/init_test.go
@@ -0,0 +1,10 @@
+package sync
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestInit(t *testing.T) {
+	fmt.Println("Sync testing")
+}
